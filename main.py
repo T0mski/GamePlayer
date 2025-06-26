9caa996ec350c042d31a794a53622942a46f9451
T0mski <thomaswaddington1@gmial.com>
2025-06-26 16:47:39 +0100

    Added the image capture and the GameImages directory this then stores an image.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pyautogui as gui
+import os
 import time
+
+from mouseinfo import screenshot
 from pynput import keyboard as kb
 import threading as th
 from GUI import GameOverlay
 import tkinter as tk
+import mss
+import mss.tools
+from PIL import Image
+import numpy as np
 
 
 running = True
@@ -12,14 +19,20 @@ BottomRight = ()
 Parameters = {
     "TopLeft": (),
     "BottomRight": (),
-    "Width": 0,
-    "Height": 0
+    "Width": 0.0,
+    "Height": 0.0,
+    "Calculated": False
+
 }
 
+
 def Update():
+    captureWindow()
     time.sleep(0.5)
 
-def listener():
+
+
+def listener_Thread():
     def on_press(key):
         global running
         global pressed
@@ -31,7 +44,6 @@ def listener():
         if key == kb.Key.ctrl_l:
             print("Storing first")
             Parameters["TopLeft"] = gui.position()
-
         if key == kb.Key.alt_l:
             print("Storing second")
             Parameters["BottomRight"] = gui.position()
@@ -42,6 +54,7 @@ def listener():
             Parameters["Width"] = width
             height = BottomRight.y - TopLeft.y
             Parameters["Height"] = height
+            Parameters["Calculated"] = True
             print("Calculating...")
             print(f"Width: {width} Height: {height}")
 
@@ -51,7 +64,7 @@ def listener():
 
 
 
-def loadWidget():
+def loadWidget_Thread():
     def createWindow():
         root = tk.Tk()
         app = GameOverlay(root)
@@ -60,12 +73,38 @@ def loadWidget():
 
     createWindow()
 
+def captureWindow():
+    global Parameters
+
+    if not Parameters.get("Calculated"):
+        print("Window Not Calculated... Skipping Capture")
+        return
+
+    window = {
+        "top": int(Parameters["TopLeft"][1]),
+        "left": int(Parameters["TopLeft"][0]),
+        "width": Parameters["Width"],
+        "height": Parameters["Height"]
+    }
+
+    path = os.path.join("D:\^ Code\Python\GamePlayer\GamePlayer\GameImages", "img.png")
+    with mss.mss() as sct:
+        screenshot = sct.grab(window)
+        img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
+
+    img.save(path)
+    print("Image Saved")
+
+
 if __name__ == "__main__":
-    listenerThread = th.Thread(target=listener, daemon=True)
+    listenerThread = th.Thread(target=listener_Thread, daemon=True)
     listenerThread.start()
-    windowThread = th.Thread(target=loadWidget, daemon=True)
+    windowThread = th.Thread(target=loadWidget_Thread, daemon=True)
     windowThread.start()
+    os.makedirs("D:\^ Code\Python\GamePlayer\GamePlayer\GameImages", exist_ok=True)
     while running:
         Update()
     print("Program Exited")
     print(f"Top-Left Coords: {Parameters["TopLeft"]}, Bottom-Right Coords: {Parameters["BottomRight"]}")
+
+
